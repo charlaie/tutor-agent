c3f3f42cfb45637b2ceab58c3aff550e653c8442
charlaie <work@charle.cc>
2026-05-09 15:47:41 +0800

    feat: initial copilotkit starter

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { CopilotSidebar } from "@copilotkit/react-core/v2";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Your App</h1>
+      <CopilotSidebar />
+    </main>
+  );
+}
